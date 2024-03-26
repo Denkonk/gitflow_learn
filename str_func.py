611b9819bdfe968f3e6f3bdb48f27a501a98cfc5
dkonkov <ferz5566@yandex.ru>
2024-03-27 00:30:34 +0300

    add new func str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,13 @@
 def str_upper(sentenses):
+    """
+    функция делает все буквы заглавными
+     """
     word_up = sentenses.upper()
     return word_up
+
+def str_upper_furst(w):
+    """
+    функция делает загловной каждую первую в тексте
+    """
+    word_up_furst = w.title()
+    return word_up_furst
